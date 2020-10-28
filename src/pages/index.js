d926214f602259ddc6c8623083ff230086e5e5d1
Keven <keven.santos.sz@gmail.com>
2020-10-27 21:59:35 -0300

    config

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Reader from '../components/reader'
+
+export default function index() {
+  return (
+    <div>
+      Tiexqxq
+      <Reader></Reader>
+    </div>
+  )
+}
