c60d84ecec00511148da05bb7f1e513c66a0cadc
gabrielpramos <ramos.gabriel.pereira@gmail.com>
2020-10-28 21:06:37 -0300

    Apply useFetch hook on getInitialProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import axios from 'axios';
 import React from 'react';
 
 import ReactMarkdown from '../components/Markdown/Reader';
+import { useFetch } from '../hooks/fetch';
 
 const Index = ({ markdown }) => {
   return <ReactMarkdown>{markdown}</ReactMarkdown>;
 };
 
 Index.getInitialProps = async () => {
-  const { data } = await axios.get(
+  const { data } = await useFetch(
     'https://raw.githubusercontent.com/liferay/liferay-frontend-guidelines/master/dxp/how_to_minimize_bleeding_themes.md',
   );
 
