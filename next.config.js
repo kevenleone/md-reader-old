6e4a0a32882862c96f53b579101e03738d57691b
Keven <keven.santos.sz@gmail.com>
2020-10-29 07:02:55 -0300

    Add clay components and imports

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 const withPlugins = require("next-compose-plugins");
+const withImages = require('next-images')
 
 const nextConfig = {
   exportPathMap: false
 };
 
+const images = withImages({
+  esModules: true,
+})
+
 module.exports = withPlugins(
-  [],
+  [images],
   nextConfig
 );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import '../../styles/globals.css';
+import '@clayui/css/lib/css/atlas.css';
+
+import { ClayIconSpriteContext } from '@clayui/icon';
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const spritemap = require('../assets/spritemap.svg');
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ClayIconSpriteContext.Provider value={spritemap}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ClayIconSpriteContext.Provider>
+  );
 }
 
 export default MyApp;
